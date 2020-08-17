654668106f8532278518e44f0da8ab25472e4492
akash062 <ripon_aiub@yahoo.com>
2020-08-17 23:56:52 +0200

    C-sharp file

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter an integer values");
+            int times = Int32.Parse(Console.ReadLine());
+          
+            for(int count=1; count<=times; count++)
+            {
+                Console.WriteLine("");
+                for(int next=1; next<=times; next++)
+                {
+                    if(next % 2 == 0)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("X");
+                    }
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
